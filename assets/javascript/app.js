b2940c7681630fe84d62585fac1e885b2ec828fc
Sunny <sundeepkmanik@gmail.com>
2018-03-28 23:32:25 -0700

    adding conditions for incorrect/correct

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,51 +25,38 @@ var questionArr = [
 // var that remembers what question we're on
 var questionIndex = 0;
 
-// function that renders questions on the page
+// var that stores correct answers
+var correctA = 0;
+
+// var that stores incorrect answers
+var incorrectA = 0;
+
+// function that displays questions on the page
 function renderQuestion() {
 
     if (questionIndex <= (questionArr.length - 1)) {
 
-        // generates display
+        // display
         $("#question-panel").text(questionArr[questionIndex].text);
         $("#o-a .opt").text(questionArr[questionIndex].options[0]);
         $("#o-b .opt").text(questionArr[questionIndex].options[1]);
         $("#o-c .opt").text(questionArr[questionIndex].options[2]);
         $("#o-d .opt").text(questionArr[questionIndex].options[3]);
-
         console.log("Game is still going!")
-
-        // activates when user selects answer
-        $(".option-list").on("click", ".list-group-item", function() {
-
-            // turns event off after one performance
-            $(".option-list").off("click", ".list-group-item");
-            console.log("Answer clicked!");
-
-            // if answer is correct
-            if ($(this).attr("id") === questionArr[questionIndex].answer) {
-
-                $(this).addClass("correct");
-                console.log("Plankton voice: CORRRREECT!");
-
-            }
-
-            // if answer is false
-            else {
-
-                $(this).addClass("incorrect");
-                $("#" + questionArr[questionIndex].answer).addClass("correct");
-                console.log("Dwight Schrute voice: FALSE!");
-
-            }
-
-        });
 
     }
 
     else {
 
-        console.log("Game over hijo!")
+        // display
+        $("#question-panel").text("Game's over! Try again?");
+        $(".o-label").detach();
+        $("#o-c .opt").detach();
+        $("#o-d .opt").detach();
+        $("#o-a .opt").text("Correct answers: " + correctA);
+        $("#o-b .opt").text("Incorrect answers: " + incorrectA);
+        console.log("Show's over, Spongebob!");
+
     }
 
 };
@@ -79,6 +66,39 @@ function renderQuestion() {
 
 renderQuestion();
 
-// document ready closing tag
+// activates when user selects answer
+$(".option-list").on("click", ".list-group-item", function() {
 
+    // turns event off after one performance
+    $(".option-list").off("click", ".list-group-item");
+    console.log("Answer clicked!");
+
+    // if answer is correct
+    if ($(this).attr("id") === questionArr[questionIndex].answer) {
+
+        correctA++;
+
+        // display
+        $("#question-panel").text("A: " + questionArr[questionIndex].answer);
+        $(this).addClass("correct");
+        console.log("Plankton voice: CORRRREECT!");
+
+    }
+
+    // if answer is false
+    else {
+
+        incorrectA++;
+
+        // display
+        $("#question-panel").text("A: " + questionArr[questionIndex].answer);
+        $(this).addClass("incorrect");
+        $("#" + questionArr[questionIndex].answer).addClass("correct");
+        console.log("Dwight Schrute voice: FALSE!");
+
+    }
+
+});
+
+// document ready closing tag
 });
